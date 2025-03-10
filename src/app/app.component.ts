a58b714de55579199c3fe005813a8ce8bebba20b
nyaro-fluentech <nyaro.andriamparany@fluentech-group.com>
2025-03-10 09:52:28 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'm1p12mean-Ny_Aro-Randy_frontend';
+}
