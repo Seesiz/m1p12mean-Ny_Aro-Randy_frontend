2f3fc0a0e37e9362fe863c78579fc4e5ed15026e
nyarodina <nyarodina@gmail.com>
2025-03-10 16:39:22 +0300

    feat: adding guard for route

--- src/app/back-office/back-office.component.ts
+++ src/app/back-office/back-office.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-back-office',
   standalone: false,
   templateUrl: './back-office.component.html',
-  styleUrl: './back-office.component.css'
+  styleUrl: './back-office.component.css',
 })
 export class BackOfficeComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
 }
---
+++ src/types/roles.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  manager = 'MANAGER',
+  client = 'CLIENT',
+  mechanic = 'MECHANIC',
+}
