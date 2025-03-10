cc98c4503d1b24694cc3eb86338117b3394d9026
nyarodina <nyarodina@gmail.com>
2025-03-10 12:57:04 +0300

    feat: initialize back-office module

---
+++ src/app/back-office/back-office.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-office',
+  standalone: false,
+  templateUrl: './back-office.component.html',
+  styleUrl: './back-office.component.css'
+})
+export class BackOfficeComponent {
+
+}
