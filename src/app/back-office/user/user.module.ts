ef304677d5aaac4f24ec327f1f155e014bca3d48
nyarodina <nyarodina@gmail.com>
2025-03-11 18:02:10 +0300

    ui: list and form design

--- src/app/back-office/user/user.module.ts
+++ src/app/back-office/user/user.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user.routing.module';
+import { UI_MODULE_IMPORTS } from '@/types/ui.module.import';
+import { ListComponent } from './list/list.component';
+import { ModalComponent } from './modal/modal.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, UserRoutingModule],
+  declarations: [ListComponent, ModalComponent],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    TranslateModule,
+    ...UI_MODULE_IMPORTS,
+  ],
 })
 export class UserModule {}
