ff915c391874001bf470fb378656b1715fe1d83f
nyarodina <nyarodina@gmail.com>
2025-03-12 17:00:18 +0300

    ui: login

--- src/app/back-office/back-office.module.ts
+++ src/app/back-office/back-office.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BackOfficeRoutingModule } from './back-office-routing.module';
 import { BackOfficeComponent } from './back-office.component';
 import { StatistiqueClientComponent } from './statistique-client/statistique-client.component';
@@ -24,6 +25,7 @@ import {
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     BackOfficeRoutingModule,
     TranslateModule,
     ...UI_MODULE_IMPORTS,
--- src/app/back-office/components/login/login.component.ts
+++ src/app/back-office/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private translate: TranslateService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  setTranslation(lang: string) {
+    this.translate.use(lang);
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+    }
+  }
+}
