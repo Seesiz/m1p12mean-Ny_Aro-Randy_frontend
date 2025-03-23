e72c67181b2191acd47d2f280937134facd36caf
randy201 <108166894+randy201@users.noreply.github.com>
2025-03-23 07:59:45 +0300

    feat(service): fix service pack integration

--- src/app/back-office/rendez-vous/info/info.component.ts
+++ src/app/back-office/rendez-vous/info/info.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IRendez_vous } from '@/types/output';
+import { viewChild } from '@angular/core';
+import { BrnDialogComponent } from '@spartan-ng/brain/dialog';
 @Component({
   selector: 'app-info',
   standalone: false,
@@ -7,15 +9,22 @@ import { IRendez_vous } from '@/types/output';
   styleUrl: './info.component.css',
 })
 export class InfoComponent {
+  public viewchildDialogRef = viewChild(BrnDialogComponent);
+
   @Input() selectedRDV: IRendez_vous | null = null;
   @Output() confirm = new EventEmitter<string>();
   @Output() refuse = new EventEmitter<string>();
+  isSubmit: boolean = false;
 
   onConfirm() {
+    this.isSubmit = true;
+    this.viewchildDialogRef()?.close({});
     if (this.selectedRDV) this.confirm.emit(this.selectedRDV._id);
   }
 
   onRefuse() {
+    this.isSubmit = true;
+    this.viewchildDialogRef()?.close({});
     if (this.selectedRDV) this.refuse.emit(this.selectedRDV._id);
   }
 }
