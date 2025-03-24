2bbfe2823b27621f2741fb2b99a2624881b94bf8
randy201 <108166894+randy201@users.noreply.github.com>
2025-03-24 08:31:12 +0300

    feat(prestation): add integration for new service

--- src/app/back-office/prestation/modal/modal.component.ts
+++ src/app/back-office/prestation/modal/modal.component.ts
@@ -1,11 +1,44 @@
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { PrestationService } from '@/app/back-office/services/prestation/prestation.service';
+import { IPrestation } from '@/types/output';
+import { viewChild } from '@angular/core';
+import { BrnDialogComponent } from '@spartan-ng/brain/dialog';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-modal',
   standalone: false,
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
 export class ModalComponent {
+  addForm: FormGroup;
+  public viewchildDialogRef = viewChild(BrnDialogComponent);
 
+  constructor(
+    private fb: FormBuilder,
+    private prestationService: PrestationService
+  ) {
+    this.addForm = this.fb.group({
+      label: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [0, [Validators.required]],
+      duree: [0, [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.addForm.valid) {
+      const prestation: IPrestation = {
+        ...this.addForm.value,
+      };
+      this.prestationService.savePrestation(prestation).then(
+        () => {
+          this.viewchildDialogRef()?.close({});
+        },
+        (error) => {
+          console.error('Error updating user:', error);
+        }
+      );
+    }
+  }
 }
