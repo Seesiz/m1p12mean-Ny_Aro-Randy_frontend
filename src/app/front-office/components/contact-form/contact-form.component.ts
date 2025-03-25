390a450819208fd010e880e1786ed07dc0c2350a
randy201 <108166894+randy201@users.noreply.github.com>
2025-03-25 20:54:03 +0300

    fix(rendez_vous): add integration to rendez-vous

--- src/app/front-office/components/contact-form/contact-form.component.ts
+++ src/app/front-office/components/contact-form/contact-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RendezVousService } from '@/app/back-office/services/rendez_vous/rendez-vous.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -11,13 +12,17 @@ export class ContactFormComponent {
   form: FormGroup;
   minDate: Date = new Date();
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private rendez_vousService: RendezVousService
+  ) {
     this.form = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       date: [new Date(), Validators.required],
       time: ['', Validators.required],
       message: ['', Validators.required],
+      contact: ['', Validators.required],
     });
   }
 
@@ -27,7 +32,25 @@ export class ContactFormComponent {
       const splitTime = formData.time.split(':');
       formData.date.setHours(Number(splitTime[0]), Number(splitTime[1]));
       const { time, ...dataToSend } = formData;
-      console.log('Form data:', dataToSend);
+
+      const data = {
+        date: dataToSend.date,
+        info: {
+          email: dataToSend.email,
+          fullname: dataToSend.name,
+          contact: dataToSend.contact,
+          message: dataToSend.message,
+        },
+        duree: 15,
+      };
+      this.rendez_vousService
+        .add_rendez_vous_from_client(data)
+        .then(() => {
+          this.form.reset();
+        })
+        .catch((error) => {
+          console.error('Error adding rendez-vous:', error);
+        });
     }
   }
 }
