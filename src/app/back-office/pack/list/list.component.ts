29e3e044cdf99c1db70524049a91cd5962dcb3d9
randy201 <108166894+randy201@users.noreply.github.com>
2025-03-26 04:59:44 +0300

    feature(pack) : add crud of pack

--- src/app/back-office/pack/list/list.component.ts
+++ src/app/back-office/pack/list/list.component.ts
@@ -1,11 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IPack, IUser } from '@/types/output';
+import { PackService } from '@/app/back-office/services/pack/pack.service';
+import { FormControl } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   standalone: false,
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  packs: IPack[] = [];
+  filteredPacks: IPack[] = [];
+  searchControl = new FormControl('');
+  loading: boolean = false;
+  selectedPack: IPack | null = null;
 
+  currentPage = 1;
+  itemsPerPage = 10;
+  totalItems = 0;
+  private destroy$ = new Subject<void>();
+  userConnected: IUser | null = null;
+
+  constructor(
+    private packService: PackService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadData();
+    this.userConnected = this.authService.getUserConnected();
+
+    this.searchControl.valueChanges
+      .pipe(takeUntil(this.destroy$), debounceTime(300), distinctUntilChanged())
+      .subscribe((searchTerm) => {
+        this.filterPacks(searchTerm || '');
+      });
+  }
+
+  loadData() {
+    this.loading = true;
+    this.packService
+      .getAll()
+      .then((packs) => {
+        this.packs = packs;
+        this.filterPacks(this.searchControl.value || '');
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
+
+  selectPackForUpdate(pack: IPack) {
+    this.selectedPack = pack;
+  }
+
+  deletePack(id: string): void {
+    if (confirm('Are you sure you want to delete this pack?')) {
+      this.loading = true;
+      this.packService
+        .delete(id)
+        .then(() => {
+          this.loadData();
+        })
+        .catch((error) => {
+          console.error('Error deleting pack:', error);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    }
+  }
+
+  filterPacks(searchTerm: string) {
+    this.filteredPacks = this.packs.filter((pack) =>
+      pack.label.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- src/app/back-office/services/pack/pack.service.ts
+++ src/app/back-office/services/pack/pack.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { IPack } from '@/types/output';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { environment } from '@/environments/environments';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PackService {
+  constructor() {}
 
-  constructor() { }
+  async getAll(): Promise<IPack[]> {
+    try {
+      const response: AxiosResponse<IPack[]> = await axios.get(
+        `${environment.apiUrl}/packs`
+      );
+      return response.data;
+    } catch (error) {
+      const err = error as AxiosError;
+      console.error('Erreur de connexion:', err.message);
+      throw new Error("Échec de l'authentification");
+    }
+  }
+
+  async delete(id: string): Promise<void> {
+    try {
+      await axios.delete(`${environment.apiUrl}/packs/${id}`);
+    } catch (error) {
+      const err = error as AxiosError;
+      console.error('Erreur de connexion:', err.message);
+      throw new Error("Échec de l'authentification");
+    }
+  }
 }
